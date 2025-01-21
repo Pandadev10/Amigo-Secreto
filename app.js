e370480850ba20f6b1d71c50552375455f6633fe
Anderson Lima <panda.engenheriadedados@gmail.com>
2025-01-21 15:21:45 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+let amigos = [];
+let amigosSorteados = [];
+
+function adicionarAmigo() {
+  let recebeAmigo = document.querySelector("input").value;
+  if (recebeAmigo) {
+    amigos.push(recebeAmigo);
+    atualizarListAmigos();
+    document.querySelector("input").value = "";
+  } else {
+    alert("Por favor, insira um nome");
+  }
+}
+
+function atualizarListAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li");
+    item.textContent = amigos[i];
+    listaAmigos.appendChild(item);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("A lista está vazia! Adicione amigos antes de sortear.");
+    return;
+  }
+  let nomeAleatorio = Math.floor(Math.random() * amigos.length);
+
+  if (nomesNoMaximo()){
+    alert ('nome no maximo');
+  }else if (amigosSorteados.includes(nomeAleatorio)){
+    sortearAmigo();
+  }else {
+    amigosSorteados.push(nomeAleatorio);
+  }
+
+
+  let nomeDoSorteado = amigos[nomeAleatorio];
+
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = '';
+  resultado.innerHTML = `Amigo sorteado: ${nomeDoSorteado}`;
+}
+
+function nomesNoMaximo(){
+    let totalAmigos = amigos.length;
+    let totalSorteados = amigosSorteados.length;
+   
+    return totalAmigos == totalSorteados;
+}
